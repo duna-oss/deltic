20c55b43560d48e0f2230193f5eba5c6324dc610
Frank de Jonge <info@frankdejonge.nl>
2025-10-05 18:18:56 +0100

    Add clock

---
+++ packages/clock/src/index.test.ts
@@ -0,0 +1,66 @@
+import {setTimeout as wait} from 'node:timers/promises';
+import {createTestClock, daysInMilliseconds, millisecondsBetween, SystemClock} from './index.js';
+
+describe('@deltic/clock', () => {
+    describe('clock.SystemClock', () => {
+        it('should provide the system time', async () => {
+            const clock = SystemClock;
+            const before = Date.now();
+
+            await wait(5); // ensure diff
+            const now = clock.now();
+            await wait(5); // ensure diff
+
+            const after = Date.now();
+
+            expect(now).toBeGreaterThan(before);
+            expect(now).toBeLessThan(after);
+        });
+    });
+
+    describe('clock.TestClock', () => {
+        it('should provide a fixed time', () => {
+            const clock = createTestClock(Date.parse('04 Dec 2022 15:30:45 GMT'));
+
+            expect(clock.now()).toBe(1670167845000);
+            clock.tick();
+            expect(clock.now()).toBe(1670167845001);
+        });
+
+        it('should be able to jump ahead in time', () => {
+            const clock = createTestClock(Date.parse('04 Dec 2022 15:30:45 GMT'));
+
+            expect(clock.now()).toBe(1670167845000);
+            clock.travelTo(Date.parse('09 Jan 2023 18:10:12 GMT'));
+            expect(clock.now()).toBe(1673287812000);
+        });
+
+        it('should be able to go back in time', () => {
+            const clock = createTestClock(Date.parse('04 Dec 2022 15:30:45 GMT'));
+
+            expect(clock.now()).toBe(1670167845000);
+            clock.travelTo(Date.parse('02 Nov 2021 18:10:12 GMT'));
+            expect(clock.now()).toBe(1635876612000);
+        });
+
+        it('should be able to go back in time using a date string', () => {
+            const clock = createTestClock('04 Dec 2022 15:30:45 GMT');
+
+            expect(clock.now()).toBe(1670167845000);
+            clock.travelTo('02 Nov 2021 18:10:12 GMT');
+            expect(clock.now()).toBe(1635876612000);
+        });
+    });
+
+    describe('clock.Helpers', () => {
+        it('should calculate the number of milliseconds in a day', () => {
+            expect(daysInMilliseconds(1)).toBe(86_400_000);
+            expect(daysInMilliseconds(7)).toBe(604_800_000);
+        });
+
+        it('should calculate time between dates', () => {
+            expect(millisecondsBetween(5, 8)).toEqual(3);
+            expect(() => millisecondsBetween(8, 5)).toThrow(new Error('Start time must be earlier than end time'));
+        });
+    });
+});
---
+++ packages/clock/src/index.ts
@@ -0,0 +1,68 @@
+export interface Clock {
+    now(): number,
+    date(): Date,
+}
+
+export const SystemClock: Clock = {
+    now: () => Date.now(),
+    date: () => new Date(),
+};
+
+export interface TestClock extends Clock {
+    tick(): void,
+    advance(increment: number): void,
+    travelTo(laterTime: number | string): void,
+}
+
+export const DefaultClock = process.env.NODE_ENV === 'test' ? createTestClock() : SystemClock;
+
+export function createTestClock(start: number | string = Date.now()): TestClock {
+    let now = typeof start === 'number' ? start : Date.parse(start);
+
+    return {
+        now: () => now,
+        advance: (increment: number) => now += increment,
+        date: () => new Date(now),
+        tick: () => ++now,
+        travelTo: (newTime: number | string) => {
+            now = typeof newTime === 'number' ? newTime : Date.parse(newTime);
+        },
+    };
+}
+
+/**
+ * Convert number of seconds into equivalent number of milliseconds
+ */
+export function secondsToMilliseconds(seconds: number): number {
+    return seconds * 1000;
+}
+
+/**
+ * Calculate how many milliseconds would be in the given number of days
+ */
+export function daysInMilliseconds(numberOfDays: number) {
+    // There are 86,400,000 milliseconds in one day
+    return 86_400_000 * numberOfDays;
+}
+
+/**
+ * Calculate how many milliseconds would be in the given number of hours
+ */
+export function hoursInMilliseconds(numberOfHours: number) {
+    // There are 3600,000 milliseconds in one hour
+    return 3_600_000 * numberOfHours;
+}
+
+/**
+ * Calculate how many milliseconds elapsed between startDate and endDate.
+ *
+ * @param startDate Must be earlier or equal to endDate
+ * @param endDate Must be later or equal to startDate
+ */
+export function millisecondsBetween(startDate: number, endDate: number) {
+    if (startDate > endDate) {
+        throw new Error('Start time must be earlier than end time');
+    }
+
+    return endDate - startDate;
+}
