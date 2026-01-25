1cfae18a9b5771921b7e9d94500669a87caa843d
Frank de Jonge <info@frankdejonge.nl>
2026-01-25 12:42:00 +0100

    Code formatting

--- packages/transaction-manager/src/index.ts
+++ packages/transaction-manager/src/index.ts
@@ -1,24 +1,21 @@
 export interface TransactionManager {
-    begin(): Promise<void>,
+    begin(): Promise<void>;
     inTransaction(): boolean;
     runInTransaction<R>(fn: () => Promise<R>): Promise<R>;
-    commit(): Promise<void>,
-    rollback(error?: unknown): Promise<void>,
+    commit(): Promise<void>;
+    rollback(error?: unknown): Promise<void>;
 }
 
 export class NoopTransactionManager implements TransactionManager {
-    async rollback(): Promise<void> {
-    }
+    async rollback(): Promise<void> {}
 
     inTransaction(): boolean {
         return true;
     }
 
-    async begin(): Promise<void> {
-    }
+    async begin(): Promise<void> {}
 
-    async commit(): Promise<void> {
-    }
+    async commit(): Promise<void> {}
 
     runInTransaction<R>(fn: () => Promise<R>): Promise<R> {
         return fn();
